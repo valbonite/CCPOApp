1bd068f604eb03e4f4ccc104f3620a5ff27de57e
valbonite <valentin.bonite@gmail.com>
2016-04-06 04:56:52 +0800

    updated index.php file

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 <html>
 <head>
 <meta http-equiv="refresh" content="0;url=pages/index.html">
-<title>SB Admin 2</title>
+<title>CCPO Crime Prediction</title>
 <script language="javascript">
-    window.location.href = "pages/index.html"
+    window.location.href = "pages/daily.html"
 </script>
 </head>
 <body>
-Go to <a href="pages/index.html">/pages/index.html</a>
 </body>
 </html>
