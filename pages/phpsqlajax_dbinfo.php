bd9b8fc8959ff115e486cdf20d5814c5868bc2a3
valbonite <valentin.bonite@gmail.com>
2016-04-07 01:04:03 +0800

    updated db config

--- pages/phpsqlajax_dbinfo.php
+++ pages/phpsqlajax_dbinfo.php
@@ -1,5 +1,17 @@
 <?php
 $username="root@localhost";
-$password="password";
-$database="ccpo_database";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+
+$active_group = 'default';
+$active_record = TRUE;
+
+$db['default']['hostname'] = $cleardb_server;
+$db['default']['username'] = $cleardb_username;
+$db['default']['password'] = $cleardb_password;
+$db['default']['database'] = $cleardb_db;
 ?>
