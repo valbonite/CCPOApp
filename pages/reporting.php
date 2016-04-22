5ce0231384308794065c9636a9487fc6344e5770
valbonite <valentin.bonite@gmail.com>
2016-04-22 16:02:47 +0800

    edited prediction pages

--- pages/reporting.php
+++ pages/reporting.php
@@ -619,6 +619,10 @@ mysqli_close($connection);
     <!-- Datepicker CSS -->
     <link href="../dist/css/bootstrap-datepicker.css" rel="stylesheet">
     <link href="../dist/css/bootstrap-datepicker.min.css" rel="stylesheet">
+    <script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <!-- Include Date Range Picker -->
+<script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css">
 
 
 
@@ -1126,6 +1130,12 @@ map.setMapTypeId(customMapTypeId);
                         </li>
                         <li class="heading">
                             <h4>FILTERS</h4>
+                        </li>
+                        <li>
+                            <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
+    <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;
+    <span></span> <b class="caret"></b>
+</div>
                         </li>
                         <li>
                             <a href="#"><i class="fa fa-list fa-fw"></i> Crime Type<span class="fa arrow rotate"></span></a>
@@ -1863,5 +1873,25 @@ $(".checkbox").click(function(){
 
 
 </body>
+<script type="text/javascript">
+$(function() {
 
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
+    cb(moment().subtract(29, 'days'), moment());
+
+    $('#reportrange').daterangepicker({
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+});
+</script>
 </html>
