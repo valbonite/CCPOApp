b3804cb3e12eaed82ff57ca20f398e720d6b0240
valbonite <valentin.bonite@gmail.com>
2016-04-25 05:23:53 +0800

    edited reporting

--- pages/reporting.php
+++ pages/reporting.php
@@ -810,22 +810,6 @@ map.setMapTypeId(customMapTypeId);
       console.log(marker);
   }
 
-  function downloadUrl(url, callback) {
-      var request = window.ActiveXObject ?
-      new ActiveXObject('Microsoft.XMLHTTP') :
-      new XMLHttpRequest;
-
-      request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-          request.onreadystatechange = doNothing;
-          callback(request, request.status);
-      }
-  };
-
-  request.open('GET', url, true);
-  request.send(null);
-}
-
   function show(category) {
     if (markerGroups.hasOwnProperty(category)) {
         var markersInCategory = markerGroups[category];
@@ -844,12 +828,31 @@ map.setMapTypeId(customMapTypeId);
             markersInCategory[i].setVisible(false);
         }
     }
+    console.log('something');
   }
 
     hide("MURDER");
     hide("THEFT");
     hide("ROBBERY");
     hide("ORDINANCES");
+
+  function downloadUrl(url, callback) {
+      var request = window.ActiveXObject ?
+      new ActiveXObject('Microsoft.XMLHTTP') :
+      new XMLHttpRequest;
+
+      request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+          request.onreadystatechange = doNothing;
+          callback(request, request.status);
+      }
+  };
+
+  request.open('GET', url, true);
+  request.send(null);
+}
+
+  
 
   function bindInfoWindow(marker, map, infoWindow, html) {
       google.maps.event.addListener(marker, 'click', function() {
