d9a5acf6cc9a30d799d00bba6ecaba907f899cb7
valbonite <valentin.bonite@gmail.com>
2016-04-26 06:04:33 +0800

    edited db info

--- pages/phpsqlajax_dbinfo.php
+++ pages/phpsqlajax_dbinfo.php
@@ -4,7 +4,7 @@ $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 2);
+$cleardb_db = substr($cleardb_url["path"], 1);
 
 $active_group = 'default';
 $active_record = TRUE;
