b406af5a4af40a93628d67d4fe06587da369776a
valbonite <valentin.bonite@gmail.com>
2016-04-28 04:03:15 +0800

    edited reporting form

--- pages/phpsqlajax_genxml.php
+++ pages/phpsqlajax_genxml.php
@@ -1,5 +1,6 @@
 <?php
-require("phpsqlajax_dbinfo.php");
+error_reporting(E_ALL);
+require('connection.php');
 
 // Start XML file, create parent node
 
@@ -9,29 +10,34 @@ $parnode = $dom->appendChild($node);
 
 // Opens a connection to a MySQL server
 
-$connection=@mysql_connect($cleardb_server, $cleardb_username, $cleardb_password);
-if (!$connection) {  die('Not connected : ' . mysql_error());}
+//$connection=@mysql_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//if (!$connection) {  die('Not connected : ' . mysql_error());}
 
 // Set the active MySQL database
 
-$db_selected = mysql_select_db($cleardb_db, $connection);
-if (!$db_selected) {
-  die ('Can\'t use db : ' . mysql_error());
-}
+//$db_selected = mysql_select_db($cleardb_db, $connection);
+//if (!$db_selected) {
+//  die ('Can\'t use db : ' . mysql_error());
+//}
+
+mysqli_select_db($cleardb_db, $connection);
 
 // Select all the rows in the markers table
 
-$query = "SELECT * FROM master_data WHERE 1";
-$result = mysql_query($query);
+//$query = "SELECT * FROM master_data WHERE 1";
+//$result = mysql_query($query);
+$result = $connection->query("SELECT * FROM master_data WHERE 1");
+
 if (!$result) {
   die('Invalid query: ' . mysql_error());
 }
 
 header("Content-type: text/xml; charset=UTF-8");
 
+
 // Iterate through the rows, adding XML nodes for each
 
-while ($row = @mysql_fetch_assoc($result)){
+while ($row=$result->fetch_object()){
   // ADD TO XML DOCUMENT NODE
   $node = $dom->createElement("marker");
   $newnode = $parnode->appendChild($node);
