8ccd4f6f4dbf7af7e9350331f1be2b85af022c4d
valbonite <valentin.bonite@gmail.com>
2016-04-28 08:45:29 +0800

    edited form

--- pages/reporting.php
+++ pages/reporting.php
@@ -1967,11 +1967,61 @@ $('#map-picker').on('shown.bs.modal', function () {
 });
 </script>
 
-<script type="text/javascript">
+<!--<script type="text/javascript">
 $('#openBtn').click(function(){
     $('#form-entry').modal({show:true})
 });
-</script>
+</script>-->
+
+<script type='text/javascript'>
+        
+        $(document).ready(function() {
+        
+            $('#openBtn').click(function(){
+    $('#form-entry').modal({show:true})
+});
+
+
+$('.modal').on('hidden.bs.modal', function( event ) {
+                $(this).removeClass( 'fv-modal-stack' );
+                $('body').data( 'fv_open_modals', $('body').data( 'fv_open_modals' ) - 1 );
+                });
+
+
+$( '.modal' ).on( 'shown.bs.modal', function ( event ) {
+                   
+                   // keep track of the number of open modals
+                   
+                   if ( typeof( $('body').data( 'fv_open_modals' ) ) == 'undefined' )
+                   {
+                     $('body').data( 'fv_open_modals', 0 );
+                   }
+                   
+                     
+                   // if the z-index of this modal has been set, ignore.
+                        
+                if ( $(this).hasClass( 'fv-modal-stack' ) )
+                        {
+                        return;
+                        }
+                   
+                $(this).addClass( 'fv-modal-stack' );
+
+                $('body').data( 'fv_open_modals', $('body').data( 'fv_open_modals' ) + 1 );
+
+                $(this).css('z-index', 1040 + (10 * $('body').data( 'fv_open_modals' )));
+
+                $( '.modal-backdrop' ).not( '.fv-modal-stack' )
+                        .css( 'z-index', 1039 + (10 * $('body').data( 'fv_open_modals' )));
+
+
+                $( '.modal-backdrop' ).not( 'fv-modal-stack' )
+                        .addClass( 'fv-modal-stack' ); 
+
+                 });
+
+        
+        });
 
 <script>
 $('.fa.arrow').on('click', function() {
