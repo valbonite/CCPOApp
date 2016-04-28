a2aa66cc71fd22c725edefd9cfc3dfe1efc08223
valbonite <valentin.bonite@gmail.com>
2016-04-28 23:26:28 +0800

    edited phpsqlajax_genxml.php file

--- pages/phpsqlajax_genxml.php
+++ pages/phpsqlajax_genxml.php
@@ -1,7 +1,8 @@
 <?php
-error_reporting(-1);
+//ini_set('display_startup_errors',1);
+//ini_set('display_errors',1);
+//error_reporting(-1);
 require("phpsqlajax_dbinfo.php");
-echo "Hello!";
 
 // Start XML file, create parent node
 
@@ -9,26 +10,19 @@ $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
 $parnode = $dom->appendChild($node);
 
-// Opens a connection to a MySQL server
-var_dump($cleardb_url);
-$connection=@mysql_connect ($cleardb_server, $cleardb_username, $cleardb_password);
-if (!$connection) {  die('Not connected : ' . mysql_error());}
-echo "Error!";
-// Set the active MySQL database
-var_dump($cleardb_url);
-var_dump($connection);
 
-$db_selected = mysql_select_db($cleardb_db, $connection);
-if (!$db_selected) {
-  die ('Can\'t use db : ' . mysql_error());
-}
+// Opens a connection to a MySQL server
+
+$connection=mysqli_connect ('us-cdbr-iron-east-03.cleardb.net', 'bdc69c2b521cb1', '3e21324b', 'heroku_1dc2f276f9a9d07');
+if (!$connection) {  die('Not connected : ' . mysql_error());}
+
 
 // Select all the rows in the markers table
 
 $query = "SELECT * FROM master_data WHERE 1";
 $result = mysqli_query($connection, $query);
 if (!$result) {
-  die('Invalid query: ' . mysql_error());
+  die('Invalid query: ' . mysqli_error($connection));
 }
 
 header("Content-type: text/xml; charset=UTF-8");
@@ -39,13 +33,16 @@ while ($row = mysqli_fetch_assoc($result)){
   // ADD TO XML DOCUMENT NODE
   $node = $dom->createElement("marker");
   $newnode = $parnode->appendChild($node);
+  $newnode->setAttribute("date", utf8_encode($row['date']));
   $newnode->setAttribute("day", utf8_encode($row['day']));
-  $newnode->setAttribute("barangay", utf8_encode($row['barangay']));
-  $newnode->setAttribute("classification", utf8_encode($row['classification']));
+  $newnode->setAttribute("time", utf8_encode($row['time']);
   $newnode->setAttribute("areaofincident", utf8_encode($row['areaofincident']));
+  $newnode->setAttribute("barangay", utf8_encode($row['barangay']));
   $newnode->setAttribute("latitude", utf8_encode($row['latitude']));
   $newnode->setAttribute("longitude", utf8_encode($row['longitude']));
   $newnode->setAttribute("crimetype", utf8_encode($row['crimetype']));
+  $newnode->setAttribute("crimecategory", utf8_encode($row['crimecategory']));
+  $newnode->setAttribute("classification", utf8_encode($row['classification']));
 }
 
 echo $dom->saveXML();
